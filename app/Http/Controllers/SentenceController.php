f890454bd8fb649bfbb6c9dc0ddf7ef4b40fe3c1
yamaryosan <canda3267@gmail.com>
2024-04-07 20:47:06 +0900

    [delete] REST APIのためcreateとedit削除

--- app/Http/Controllers/SentenceController.php
+++ app/Http/Controllers/SentenceController.php
@@ -15,14 +15,6 @@ class SentenceController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
@@ -34,14 +26,6 @@ class SentenceController extends Controller
      * Display the specified resource.
      */
     public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
     {
         //
     }
