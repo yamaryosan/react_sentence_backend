dc403086f99e836220a7a6030ceaa6fa3f7b88c5
yamaryosan <canda3267@gmail.com>
2024-04-11 07:47:46 +0900

    [add] 文章用ルート追加

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\SentenceController;
+use App\Http\Controllers\ArticleController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// 文章用ルート
 Route::resource('/sentences', SentenceController::class);
+// 記事用ルート
+Route::resource('/articles', ArticleController::class);
 // 文章アップロード用ルート
 Route::post('/sentences/upload', [SentenceController::class, 'upload']);
 // 記事アップロード用ルート
