f21fcc194b7c2133e7b4a2720bbdcc99859850fe
yamaryosan <canda3267@gmail.com>
2024-04-14 00:27:07 +0900

    [add] 記事全削除ルートを追加

--- routes/api.php
+++ routes/api.php
@@ -10,11 +10,14 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// 文章用ルート
-Route::resource('/sentences', SentenceController::class);
-// 記事用ルート
-Route::resource('/articles', ArticleController::class);
 // 文章アップロード用ルート
 Route::post('/sentences/upload', [SentenceController::class, 'upload']);
+// 文章用ルート
+Route::resource('/sentences', SentenceController::class);
+
 // 記事アップロード用ルート
 Route::post('/articles/upload', [ArticleController::class, 'upload']);
+// 記事全削除用ルート
+Route::delete('/articles/deleteAll', [ArticleController::class, 'truncate']);
+// 記事用ルート
+Route::resource('/articles', ArticleController::class);
