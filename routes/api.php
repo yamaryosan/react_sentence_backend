82e644c1e2379094654207b78375ca70c9fdc2f7
yamaryosan <canda3267@gmail.com>
2024-04-20 19:57:15 +0900

    [update] 認証情報用パス、記事検索用パスをweb.phpに作成

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\SentenceController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -17,8 +18,7 @@ Route::resource('/sentences', SentenceController::class);
 
 // 記事アップロード用ルート
 Route::post('/articles/upload', [ArticleController::class, 'upload']);
-// 記事検索用ルート
-Route::get('/search', [ArticleController::class, 'search']);
+
 // 記事全削除用ルート
 Route::delete('/articles/deleteAll', [ArticleController::class, 'truncate']);
 // 記事用ルート
