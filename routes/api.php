792b08397e1f0047126d8f1663cab72d8f69072b
yamaryosan <canda3267@gmail.com>
2024-06-09 00:00:58 +0900

    [create] 問い合わせのためのルートを追加

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\SentenceController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ArticleImageController;
+use App\Http\Controllers\ContactController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -32,3 +33,6 @@ Route::resource('/articles', ArticleController::class);
 // 記事画像アップロード用ルート
 Route::post('/articleImages/upload', [ArticleImageController::class, 'upload']);
 Route::delete('/articleImages/deleteAll', [ArticleImageController::class, 'truncate']);
+
+// 問い合わせ用ルート
+Route::post('/contacts', [ContactController::class, 'store']);
