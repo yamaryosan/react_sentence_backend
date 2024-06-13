aac55fbf6ee6d37243afee17d74b24f12aef1b4c
yamaryosan <canda3267@gmail.com>
2024-06-14 07:47:07 +0900

    [add] S3テスト用ルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\SentenceController;
 use App\Http\Controllers\AdminController;
 
+use App\Http\Controllers\ExampleController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,3 +17,8 @@ Route::get('/verify', [AdminController::class, 'checkAdmin']);
 Route::get('/articles/search', [ArticleController::class, 'search']);
 // 文章検索用ルート
 Route::get('/sentences/search', [SentenceController::class, 'search']);
+
+// S3アップロード用ルート
+Route::get('/uploadToS3', [ExampleController::class, 'uploadToS3']);
+Route::get('/downloadFromS3', [ExampleController::class, 'downloadFromS3']);
+Route::get('/showImage', [ExampleController::class, 'showImage']);
