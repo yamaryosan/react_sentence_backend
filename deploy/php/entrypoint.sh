a31568e4caf0f95033dd3073c8407fcceae68f7f
yamaryosan <canda3267@gmail.com>
2024-10-20 19:39:18 +0900

    [fix] さらに場合分け

--- deploy/php/entrypoint.sh
+++ deploy/php/entrypoint.sh
@@ -18,8 +18,12 @@ else
     echo "Sessions table already exists"
 fi
 
-# マイグレーションの実行
-php artisan migrate --force
+# マイグレーションの実行(すでにマイグレーションが存在する場合は実行しない)
+if [ ! -f database/migrations/*_create_users_table.php ]; then
+    php artisan migrate
+else
+    echo "Migrations already exist"
+fi
 
 # キャッシュのクリア
 php artisan config:cache
