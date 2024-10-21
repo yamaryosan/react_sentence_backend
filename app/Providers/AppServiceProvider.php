0d4523d50c956c6f9f6d0a2cf880665579019b30
yamaryosan <canda3267@gmail.com>
2024-10-21 22:54:45 +0900

    [add] ログに環境変数を記載

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \Log::info('env: ', [
+            'APP_ENV' => env('APP_ENV'),
+            'DB_CONNECTION' => env('DB_CONNECTION'),
+            'DB_DATABASE' => env('DB_DATABASE'),
+            'DB_USERNAME' => env('DB_USERNAME'),
+            'DB_PASSWORD' => env('DB_PASSWORD'),
+        ]);
     }
 }
