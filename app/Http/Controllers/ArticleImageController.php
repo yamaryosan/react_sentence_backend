7a2fe01814b8513c54fd0493002870207e8cddd5
yamaryosan <canda3267@gmail.com>
2025-03-29 15:15:10 +0900

    add: 非同期で画像アップロード

--- app/Http/Controllers/ArticleImageController.php
+++ app/Http/Controllers/ArticleImageController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+
+use App\Jobs\UploadImagesJob;
 class ArticleImageController extends Controller
 {
     /**
@@ -16,13 +18,28 @@ class ArticleImageController extends Controller
         if (empty($images)) {
             return response()->json(['message' => 'ファイルが存在しません'], 400);
         }
-        // imagesディレクトリがS3に存在しない場合は作成する
-        if (!Storage::disk('s3')->exists('images/')) {
-            Storage::disk('s3')->put('images/.gitkeep', '');
+
+        // ファイル群を一時保存
+        $imageDir = storage_path('app/images');
+        //以前のapp/imagesのディレクトリとファイルを削除
+        $previousFiles = glob($imageDir . '/*');
+        foreach ($previousFiles as $previousFile) {
+            unlink($previousFile);
         }
+        // app/imagesのディレクトリを削除
+        if (is_dir($imageDir)) {
+            rmdir($imageDir);
+        }
+        // app/imagesのディレクトリを作成
+        mkdir($imageDir);
+        // ファイルを一時保存
         foreach ($images as $image) {
-            Storage::disk('s3')->putFileAs('images/', $image, $image->getClientOriginalName());
+            $image->move($imageDir, $image->getClientOriginalName());
         }
+
+        // 画像をS3にアップロード
+        UploadImagesJob::dispatch($imageDir);
+
         return response()->json(['message' => '画像をアップロードしました']);
     }
 
